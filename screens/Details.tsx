22e80e656369c56391a7c4b04fde98f37c0b4cb8
Suyash Pradhan <pradhansuyash39@gmail.com>
2024-12-03 14:48:26 +0530

    feat: added navigation methods

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -8,6 +8,18 @@ const Details = () => {
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text style={{textAlign: "center", fontSize: 40}}>Details Page</Text>
             <Pressable style={{backgroundColor: "black", marginTop: 12, padding: 12, borderRadius: 4}}
+                       onPress={() => navigation?.push('Details')}>
+                <Text style={{fontSize: 16, color: "white"}}>Go to Details Page Again...</Text>
+            </Pressable>
+            <Pressable style={{backgroundColor: "black", marginTop: 12, padding: 12, borderRadius: 4}}
+                       onPress={() => navigation?.popTo('Detail"')}>
+                <Text style={{fontSize: 16, color: "white"}}>Go to Home</Text>
+            </Pressable>
+            <Pressable style={{backgroundColor: "black", marginTop: 12, padding: 12, borderRadius: 4}}
+                       onPress={() => navigation?.popToTop()}>
+                <Text style={{fontSize: 16, color: "white"}}>Go to first screen</Text>
+            </Pressable>
+            <Pressable style={{backgroundColor: "black", marginTop: 12, padding: 12, borderRadius: 4}}
                        onPress={() => navigation?.goBack()}>
                 <Text style={{fontSize: 16, color: "white"}}>Home</Text>
             </Pressable>
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text style={{textAlign: "center", fontSize: 40}}>Home Page</Text>
             <Pressable style={{backgroundColor: "black", marginTop: 12, padding: 12, borderRadius: 4}}
-                       onPress={() => navigation?.navigate('Details')}>
+                       onPress={() => navigation?.push('Details')}>
                 <Text style={{fontSize: 16, color: "white"}}>Details Page</Text>
             </Pressable>
         </View>
