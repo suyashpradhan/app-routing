5bc0bab67266ec8f8fe7b75adf56dfd5ebab8368
Suyash Pradhan <pradhansuyash39@gmail.com>
2025-05-12 23:39:31 +0530

    fixes dependency error

--- App.tsx
+++ App.tsx
@@ -1,31 +1,8 @@
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {NavigationContainer} from "@react-navigation/native";
-import Details from "./screens/Details";
-import {ProductScreen} from "./screens/ProductScreen";
-
-const Stack = createNativeStackNavigator()
-
-const RootStack = () => {
-    return (
-        <Stack.Navigator initialRouteName="Products" screenOptions={{
-            headerStyle: {
-                backgroundColor: "white"
-            },
-            headerTintColor: "black",
-        }}>
-            <Stack.Screen name="Products" options={{title: "Products"}} component={ProductScreen}/>
-            <Stack.Screen name="Details" component={Details} options={({route}) => ({
-                headerBackVisible: true, title: `Product Details`
-            })}/>
-        </Stack.Navigator>
-    )
-}
+import {Text} from "react-native";
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <RootStack/>
-        </NavigationContainer>
+        <Text>Hello World!</Text>
     );
 }
 
--- screens/Details.tsx
+++
@@ -1,9 +0,0 @@
-import {ProductDetails} from "../components/ProductDetails";
-
-const Details = ({route}: { route: any }) => {
-    const itemId = route.params.itemId;
-
-    return <ProductDetails itemId={itemId}/>
-}
-
-export default Details;
