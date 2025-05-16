019620566654f4280fef1115a1b5d3eb3653935c
Suyash Pradhan <pradhansuyash39@gmail.com>
2025-05-16 23:05:29 +0530

    tab and drawer navigation

--- navigation/screens/Notifications.tsx
+++ navigation/screens/Notifications.tsx
@@ -1,15 +1,13 @@
-import {Button, Text, View} from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { Button, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export const Notifications = ({route}:{route: any})=>{
-    const navigation = useNavigation();
+export const Notifications = () => {
+  const navigation = useNavigation();
 
-    const {id} = route.params;
-    return (
-        <View>
-            <Text>Notifications</Text>
-            <Text>{id}</Text>
-            <Button title="Back" onPress={() => navigation.goBack()}/>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Notifications</Text>
+      <Button title="Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
--- navigation/screens/Profile.tsx
+++ navigation/screens/Profile.tsx
@@ -1,16 +1,29 @@
-import {Button, Text, View} from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { Button, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "./Home";
+import { Notifications } from "./Notifications";
 
-export const Profile = ({route}:{route: any})=>{
-    const navigation = useNavigation();
+export const Profile = ({ route }: { route: any }) => {
+  const navigation = useNavigation();
 
-    const {userId}  = route.params;
+  const { userId } = route.params;
 
-    return (
-        <View>
-            <Text>Profile</Text>
-            <Text>{userId}</Text>
-            <Button title="Notifications" onPress={()=>navigation.navigate("Notifications",{id: userId})}  />
-        </View>
-    )
-}
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Text>{userId}</Text>
+      <Button
+        title="Notifications"
+        onPress={() => navigation.navigate("Notifications")}
+      />
+
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Notifications" component={Notifications} />
+      </Tab.Navigator>
+    </View>
+  );
+};
